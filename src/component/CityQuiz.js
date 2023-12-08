de228b456be12352ad9f314fbaf54bba40258ecd
minghzhang <minghzhang@163.com>
2023-12-08 21:52:27 +0800

    vv

---
+++ src/component/CityQuiz.js
@@ -0,0 +1,20 @@
+export default function Form({status = 'empty'}) {
+    if (status === 'success') {
+        return <h1>That's right!</h1>;
+    }
+
+    return (
+        <>
+            <h2>City Quiz</h2>
+            <p>In which city is there a billboard that turns air into drinkable water?</p>
+            <form>
+                <textarea disabled={status === 'submitting'}/>
+                <br/>
+                <button disabled={status === 'empty' || status === 'submitting'}>Submit</button>
+            </form>
+            {status === 'error' && <p className="Error">
+                Good guess but a wrong answer. Try again!
+            </p>}
+        </>
+    );
+}
